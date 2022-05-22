e547829d452f4b8de88b1823dd779a4c38245933
Bryan MacFarlane <bryanmacf@gmail.com>
2022-05-21 19:30:17 -0500

    svg buttons

--- views/hero.tsx
+++ views/hero.tsx
@@ -17,12 +17,12 @@ export function Hero() {
                   <h1 className={tw`font-bold text-5xl mb-4`}>Deno SSR + React + Twind</h1>
                   <h2 className={tw`font-bold text-2xl mb-12`}>Composable React using Tailwind via Twind</h2>
                   <div>
-                      <a href="https://github.com/bryanmacfarlane/deno-react-ssr" className={tw`bg-blue-500 rounded-md font-bold text-white text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-600 mr-2`}>
-                          Repository
+                      <a href="https://github.com/bryanmacfarlane/deno-react-ssr" className={tw`bg-green-600 rounded-md font-bold text-white text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-red-600 ml-2`}>
+                        <img className={tw`h-8 inline w-auto px-3`} src="/img/github.svg" alt=""/>Repo
                       </a>
-                      <a href="https://deno.land" className={tw`bg-red-500 rounded-md font-bold text-white text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-red-600 ml-2`}>
-                          Deno
-                      </a>
+                      <a href="https://github.com/bryanmacfarlane/deno-react-ssr" className={tw`bg-blue-600 rounded-md font-bold text-white text-center px-4 py-3 transition duration-300 ease-in-out hover:bg-red-600 ml-2`}>
+                        <img className={tw`h-8 inline w-auto px-3`} src="/logo.svg" alt=""/>Deno
+                      </a>                      
                   </div>
               </div>
           </div>
